d28c68c1735bc549bfb28dcac5524e8dfe87b4dc
carbonscott <wangimagine@gmail.com>
2015-03-15 15:09:20 +0800

    upload3

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ./chapters/c*\.md ref.md > main.md && pandoc -V documentclass=report -V fontsize=12pt -H header.sty main.md -o output.pdf --latex-engine=xelatex
+cat ./chapters/c*\.md ref.md > main.md && pandoc --toc -V documentclass=report -V fontsize=12pt -H header.sty main.md -o output.pdf --latex-engine=xelatex
